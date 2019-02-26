5a69a02c75afa443a414f81615b389d1e73a151c
Daniel Junco <dsjunco@gmail.com>
2019-02-26 23:00:16 +0100

    add User Associations: Routes

--- routes/routes.js
+++ routes/routes.js
@@ -16,17 +16,22 @@ router.get("/", function(req, res){
 });
 
 //NEW - show register form
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("routes/new");
 });
 
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author:author};
+
     //Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -51,5 +56,14 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 module.exports = router;
